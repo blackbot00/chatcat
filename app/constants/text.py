73508b7a6b2b126e777b6bf5b80d796b0758a7f4
blackbot00 <109160013+blackbot00@users.noreply.github.com>
2026-01-30 07:01:56 +0530

    Create text.py

---
+++ app/constants/text.py
@@ -0,0 +1,14 @@
+START_TEXT = (
+    "💖 *Coupledatingbot*\n"
+    "━━━━━━━━━━━━━━━\n"
+    "Welcome 🤗\n"
+    "Let’s find your perfect connection 💕"
+)
+
+CHOOSE_CHAT = (
+    "💬 *Who do you want to chat with?*\n"
+    "━━━━━━━━━━━━━━━"
+)
+
+ADMIN_ONLY = "🚫 This command is only for Admin"
+AI_DISABLED = "🚫 AI chat is temporarily disabled. Try again later..."
