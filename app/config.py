f9df2b91167ecd69ec0e7e5ea67b13336df017a3
blackbot00 <109160013+blackbot00@users.noreply.github.com>
2026-01-30 06:58:45 +0530

    Create config.py

---
+++ app/config.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+MONGO_URI = os.getenv("MONGO_URI")
+OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
+
+CASHFREE_APP_ID = os.getenv("CASHFREE_APP_ID")
+CASHFREE_SECRET_KEY = os.getenv("CASHFREE_SECRET_KEY")
+CASHFREE_WEBHOOK_SECRET = os.getenv("CASHFREE_WEBHOOK_SECRET")
+
+BASE_URL = os.getenv("BASE_URL")
+WEBHOOK_SECRET = os.getenv("WEBHOOK_SECRET")
+
+ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS", "").split(",")))
